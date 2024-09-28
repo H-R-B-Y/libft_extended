736b97421aeb899bb56231bd8f17662dbd58bcec
Harvey Breeze <hbreeze@z2t17c4.42bangkok.com>
2024-09-28 19:03:49 +0700

    Added get next line

---
+++ get_next_line_utils.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbreeze <hbreeze@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 13:47:00 by hbreeze           #+#    #+#             */
+/*   Updated: 2024/09/28 19:01:36 by hbreeze          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin( char *s1, char *s2, int flag)
+{
+	char	*output;
+	size_t	size1;
+	size_t	size2;
+	size_t	index;
+
+	size1 = ft_strlen(s1);
+	size2 = ft_strlen(s2);
+	index = 0;
+	if (!(size1 + size2))
+		return (0);
+	output = malloc(size1 + size2 + 1);
+	if (!output)
+		return (0);
+	output[size1 + size2] = '\0';
+	while (s1 && index < size1)
+		output[index++] = (char)*s1++;
+	index = 0;
+	while (s2 && index < size2)
+		output[size1 + index++] = (char)*s2++;
+	if ((flag & (1 << 1)) == (1 << 1))
+		free(s1 - size1);
+	if ((flag & (1 << 2)) == (1 << 2))
+		free(s2 - index);
+	return (output);
+}
+
+
