9db5871a56c51bba9750d0bbd0d8c80fb33b9077
H-R-B-Y <harveyraybreeze@gmail.com>
2025-09-09 16:22:41 +0100

    updated the module maker

--- math/matrix3d/matrix3d_rotation.c
+++ math/matrix3d/matrix3d_rotation.c
@@ -6,7 +6,7 @@
 /*   By: hbreeze <hbreeze@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 16:42:41 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/09/15 16:45:05 by hbreeze          ###   ########.fr       */
+/*   Updated: 2025/09/16 16:13:30 by hbreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,15 @@ t_matrix3d	matrix3d_rotation(t_vec3 rotation)
 	angles[3] = sinf(rotation.y);
 	angles[4] = cosf(rotation.z);
 	angles[5] = sinf(rotation.z);
-	res[0] = (t_matrix3d){{{1, 0, 0},
-		{0, angles[0], -angles[1]}, {0, angles[1], angles[0]}}};
-	res[1] = (t_matrix3d){{{angles[2], 0, angles[3]},
-		{0, 1, 0}, {-angles[3], 0, angles[2]}}};
-	res[2] = (t_matrix3d){{{angles[4], -angles[5], 0},
-		{angles[5], angles[4], 0}, {0, 0, 1}}};
+	res[0] = (t_matrix3d){{
+		{{1, 0, 0}}, {{0, angles[0], -angles[1]}}, {{0, angles[1], angles[0]}}
+	}};
+	res[1] = (t_matrix3d){{
+		{{angles[2], 0, angles[3]}}, {{0, 1, 0}}, {{-angles[3], 0, angles[2]}}
+	}};
+	res[2] = (t_matrix3d){{
+		{{angles[4], -angles[5], 0}}, {{angles[5], angles[4], 0}}, {{0, 0, 1}}
+	}};
 	res[3] = matrix3d_multiply(res[2], matrix3d_multiply(res[1], res[0]));
 	return (res[3]);
 }
