3a26038d3d8b3aafd8e36d633d93f48ae9a6a24a
H-R-B-Y <hbreeze@student.42.fr>
2025-10-11 12:32:32 +0100

    There was an issue with the cdll remove node function, it should be fixed now

--- cdll/cdll_remove_node.c
+++ cdll/cdll_remove_node.c
@@ -6,24 +6,34 @@
 /*   By: hbreeze <hbreeze@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 19:14:23 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/10/05 19:15:09 by hbreeze          ###   ########.fr       */
+/*   Updated: 2025/10/11 12:32:07 by hbreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cdll.h"
 
+// TODO: the head and tail of the cdll are not being set to null
+
 void	cdll_remove_node(t_cdll *list, struct s_cdll_node *node, void (*del)(void *))
 {
 	if (!list || !node || list->count == 0)
 		return ;
-	if (node->prev)
-		node->prev->next = node->next;
+	if (list->count == 1)
+	{
+		list->head = 0;
+		list->tail = 0;
+	}
 	else
-		list->head = node->next;
-	if (node->next)
+	{
 		node->next->prev = node->prev;
-	else
-		list->tail = node->prev;
+		node->prev->next = node->next;
+		if (node == list->head)
+			list->head = node->next;
+		else if (node == list->tail)
+			list->tail = node->prev;
+	}
+	node->next = 0;
+	node->prev = 0;
 	if (del)
 		del(node->data);
 	free(node);
